12ca672787baaef5eaf0d8ef8931bb7ea6fdf4bb
muhammad-awais-eng <muhammad-awais-eng>
2021-12-06 10:48:16 +0500

    header   login  with  google

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Login />} />
         </Routes>
